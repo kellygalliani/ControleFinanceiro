c420e5c71d123b143c4c29747530c53fa447d6eb
kelly galliani <kelly_92b@hotmail.com>
2022-09-30 11:14:04 -0300

    Entrega1

--- scripts/IncremetarPreço.js
+++ scripts/IncremetarPreço.js
@@ -1,21 +1,7 @@
 let totalPreco = 0
 let totalValue = document.getElementById("#totalValue")
 
-
-function incrementarPreco(cards){
-console.log(cards)
- for(let i = 0; i < cards.length; i++){
-    totalPreco = totalPreco + cards[i].value
- }
- totalValue.innerText = `R$ ${totalPreco}`
-}
-
-function decrementarPreco(idProduto){
-    for(let i = 0; i< data.length; i++){
-
-       if(idProduto == data[i].id){
-           totalPreco -= data[i].value
-       }
-    }
-precoTotalCarrinho.innerText = `R$ ${totalPreco}`
-}
+const valuesSum = insertedValues.value.reduce((previousValue, currentValue) =>{
+   totalPreco = previousValue + currentValue
+   return totalValue.innerText = `R$ ${totalPreco}`
+})
--- scripts/filtro.js
+++ scripts/filtro.js
@@ -1,5 +1,37 @@
-/* const filter = insertedValues.filter(value) => {
-
-} */
-
 let insertedValuesfiltered = [];
+
+const buttonEntradaFilter = document.getElementById("1")
+
+buttonEntradaFilter.addEventListener("click" ((insertedValues)=>{
+    
+    insertedValues.filter((value) => {
+    if (value.categoryID === 1){
+
+        insertedValuesfiltered.push(value)
+    }
+    return listCards (insertedValuesfiltered, ulListCards)
+})
+}))
+
+
+const buttonSaidaFilter = document.getElementById("2")
+const filterSaida = insertedValues.filter((value) => {
+    if (value.categoryID === 2){
+        insertedValuesfiltered.push(value)
+        
+    }
+    return listCards (insertedValuesfiltered, ulListCards)
+})
+
+const buttonTodosFilter = document.getElementById("all")
+const filterTodos = insertedValues.filter((value) => {
+    if (value.categoryID === 1 || 2){
+        insertedValuesfiltered.push(value)
+        
+        
+    }
+    return listCards (insertedValuesfiltered, ulListCards)
+})
+
+
+//Como remover os outros que não são entrada?
